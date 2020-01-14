d31af235ccb80818adff534dc45e4f366301e514
Alexandru-Paul Copil <alexandru@cpl.li>
2020-01-14 23:19:08 +0000

    Add POC web service

--- cmd/errnil/main.go
+++ cmd/errnil/main.go
@@ -1,19 +1,63 @@
-package main
+package main // import "cpl.li/go/errnil/cmd/errnil"
 
 import (
-	"fmt"
+	"go/token"
+	"log"
+	"net/http"
 	"os"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
 
 	"cpl.li/go/errnil/pkg/errnil"
 )
 
 func main() {
-	positions, err := errnil.Inspect(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
+	router := gin.New()
 
-	for _, pos := range positions {
-		fmt.Printf("%s:%d:%d\n", pos.Filename, pos.Line, pos.Column)
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status":    "ok",
+			"timestamp": time.Now().Unix(),
+		})
+	})
+
+	dir := path.Join(os.TempDir(), "errnil")
+	router.GET("/inspect", func(c *gin.Context) {
+		pkg := c.Query("pkg")
+
+		downloadPath, err := errnil.Download(pkg, dir)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		defer os.RemoveAll(downloadPath)
+
+		positions, err := errnil.Inspect(downloadPath)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		cleanPositions(positions, path.Join(downloadPath, ".."))
+
+		c.JSON(http.StatusOK, gin.H{
+			"positions_count": len(positions),
+			"positions":       positions,
+		})
+	})
+
+	log.Fatal(router.Run(":8080"))
+}
+
+func cleanPositions(positions []token.Position, tmpDir string) {
+	for idx := range positions {
+		positions[idx].Filename = strings.Replace(positions[idx].Filename, tmpDir+"/", "", 1)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module cpl.li/go/errnil
 
 go 1.13
 
-require golang.org/x/tools v0.0.0-20200113154838-30cae5f2fb06
+require (
+	github.com/gin-gonic/gin v1.5.0
+	golang.org/x/tools v0.0.0-20200113154838-30cae5f2fb06
+)
