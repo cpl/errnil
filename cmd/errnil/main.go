ac4935b1901b58f6024a46978e947c41eeb7e402
Alexandru-Paul Copil <alexandru@cpl.li>
2020-01-18 18:15:38 +0000

    Break HTML prototypes into Go templates

--- cmd/errnil/main.go
+++ cmd/errnil/main.go
@@ -1,10 +1,15 @@
 package main // import "cpl.li/go/errnil/cmd/errnil"
 
 import (
+	"errors"
+	"html/template"
 	"log"
+	"net/http"
 	"os"
 	"path"
 	"time"
+
+	gintemplate "github.com/foolin/gin-template"
 
 	"github.com/gin-gonic/gin"
 
@@ -18,6 +23,51 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Recovery())
+
+	router.HTMLRender = gintemplate.New(gintemplate.TemplateConfig{
+		Root:         path.Join("static", "views"),
+		Extension:    ".gohtml",
+		Partials:     []string{"layouts/reporesults"},
+		Funcs:        template.FuncMap{},
+		DisableCache: true,
+	})
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index", nil)
+	})
+	router.GET("/results", func(c *gin.Context) {
+		repo := c.Query("repo")
+		if repo == "" {
+			c.HTML(http.StatusBadRequest, "error", gin.H{
+				"code":    http.StatusBadRequest,
+				"message": "missing repo query param",
+			})
+			return
+		}
+
+		entry, err := downloadInspectStore(repo, downloadDir, storage)
+		if err != nil {
+			if errors.Is(err, store.ErrRepoNotFound) {
+				c.HTML(http.StatusNotFound, "error", gin.H{
+					"code":    http.StatusNotFound,
+					"message": err.Error(),
+				})
+			} else {
+				c.HTML(http.StatusInternalServerError, "error", gin.H{
+					"code":    http.StatusInternalServerError,
+					"message": err.Error(),
+				})
+			}
+			return
+		}
+
+		c.HTML(http.StatusOK, "results", gin.H{
+			"repo":          repo,
+			"positionCount": entry.PositionsCount,
+			"positions":     entry.Positions,
+			"updatedAt":     entry.UpdatedAt,
+		})
+	})
 
 	api := router.Group("/api")
 	{
